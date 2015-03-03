226a649badea232eb859dc3caeaa62e17f08f4a6
Valentijn <valentijn@mindsoup.net>
2015-03-03 19:48:32 +0100

    greatly improved stats override view

--- Android/app/src/main/java/net/mindsoup/charactersoup/fragments/CharacterInfoFragment.java
+++ Android/app/src/main/java/net/mindsoup/charactersoup/fragments/CharacterInfoFragment.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import net.mindsoup.charactersoup.CharacterActivity;
 import net.mindsoup.charactersoup.R;
@@ -23,9 +24,11 @@ import net.mindsoup.charactersoup.pf.PfCharacter;
  * @author Valentijn
  *
  */
-public class CharacterInfoFragment extends CharacterFragment {
+public class CharacterInfoFragment extends CharacterFragment implements OnClickListener {
 	
 	private PfCharacter character;
+
+    private int[] buttonIds = new int[]{R.id.xp_button, R.id.money_button, R.id.hp_button, R.id.skill_button, R.id.feat_button, R.id.spells_button};
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -52,34 +55,31 @@ public class CharacterInfoFragment extends CharacterFragment {
 			character = ca.getCharacter();
 			
 			TextView editText = (TextView)getActivity().findViewById(R.id.char_info_text);
-			String stats = "";
+            String stats = "<b>" + character.getName() + "</b><br>level " + character.getLevel() + " " + character.getRace().toString() + " " + character.getPfClass().toString();
+            editText.setText(Html.fromHtml(stats));
 			
 			EditText edit = (EditText)getActivity().findViewById(R.id.edit_xp);
 			edit.setText(Integer.toString(character.getXp()));
 			
-			Button button = (Button)getActivity().findViewById(R.id.xp_button);
-			button.setOnClickListener(new OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					setXp();					
-				}
-			});
-			
 			edit = (EditText)getActivity().findViewById(R.id.edit_money);
 			edit.setText(Integer.toString(character.getMoney()));
-			button = (Button)getActivity().findViewById(R.id.money_button);
-			button.setOnClickListener(new OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					setMoney();					
-				}
-			});
-			
-			stats += "Name: </b>" + character.getName() + "</b> (level " + character.getLevel() + " " + character.getRace().toString() + " " + character.getPfClass().toString() + ")<br>";
-			
-			editText.setText(Html.fromHtml(stats));
+
+            edit = (EditText)getActivity().findViewById(R.id.edit_hp);
+            edit.setText(Integer.toString(character.getMaxHitpoints().sum()));
+
+            edit = (EditText)getActivity().findViewById(R.id.edit_skill);
+            edit.setText(Integer.toString(character.getAvailableSkillRanks()));
+
+            edit = (EditText)getActivity().findViewById(R.id.edit_feat);
+            edit.setText(Integer.toString(character.getAvailableFeats()));
+
+            edit = (EditText)getActivity().findViewById(R.id.edit_spells);
+            edit.setText(Integer.toString(character.getAvailableSpecialPowers()));
+
+            for(int id : buttonIds) {
+                Button button = (Button) getActivity().findViewById(id);
+                button.setOnClickListener(this);
+            }
 		}
 	}
 	
@@ -117,19 +117,71 @@ public class CharacterInfoFragment extends CharacterFragment {
 		}
 	}
 	
-	protected void setMoney() {
+	protected void setMoney() throws NumberFormatException {
 		EditText edit = (EditText)getActivity().findViewById(R.id.edit_money);
 		character.setMoney(Integer.parseInt( edit.getText().toString() ));
 		updateCharacter();
 	}
 
+    protected void setHp() throws NumberFormatException {
+        EditText edit = (EditText)getActivity().findViewById(R.id.edit_hp);
+        character.setHitpoints(Integer.parseInt( edit.getText().toString() ));
+        updateCharacter();
+    }
+
+    protected void setSkills() throws NumberFormatException {
+        EditText edit = (EditText)getActivity().findViewById(R.id.edit_skill);
+        character.setAvailableSkillRanks(Integer.parseInt( edit.getText().toString() ));
+        updateCharacter();
+    }
+
+    protected void setFeats() throws NumberFormatException {
+        EditText edit = (EditText)getActivity().findViewById(R.id.edit_feat);
+        character.setAvailableFeats(Integer.parseInt( edit.getText().toString() ));
+        updateCharacter();
+    }
+
+    protected void setSpells() throws NumberFormatException {
+        EditText edit = (EditText)getActivity().findViewById(R.id.edit_spells);
+        character.setAvailableSpecialPowers(Integer.parseInt( edit.getText().toString() ));
+        updateCharacter();
+    }
+
     @Override
-    public String getHelpTitle() {
+    public String getHelpTitle() throws NumberFormatException {
         return this.getActivity().getString(R.string.info_fragment_help_title);
     }
 
     @Override
     public String getHelpText() {
         return this.getActivity().getString(R.string.info_fragment_help_text);
+    }
+
+    @Override
+    public void onClick(View view) {
+        try {
+            switch (view.getId()) {
+                case R.id.money_button:
+                    setMoney();
+                    break;
+                case R.id.xp_button:
+                    setXp();
+                    break;
+                case R.id.hp_button:
+                    setHp();
+                    break;
+                case R.id.skill_button:
+                    setSkills();
+                    break;
+                case R.id.feat_button:
+                    setFeats();
+                    break;
+                case R.id.spells_button:
+                    setSpells();
+                    break;
+            }
+        } catch (NumberFormatException e) {
+            Toast.makeText(this.getActivity(), "Invalid input", Toast.LENGTH_SHORT).show();
+        }
     }
 }
